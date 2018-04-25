4510439394330dd755e86b48e919464921807606
jinalp140 <36014243+jinalp140@users.noreply.github.com>
2018-04-25 02:01:24 -0400

    Add files via upload

--- final_food_project/script.js
+++ final_food_project/script.js
@@ -3,9 +3,7 @@ function validateform(){
 var user_name=document.myform.user_name.value;  
 var password=document.myform.password.value;  
   
-  
-  
-if (user_name==null || user_name==""){  
+  if (user_name==null || user_name==""){  
   alert("Name can't be blank");  
   return false;  
 }else if(password.length<6){  
@@ -21,3 +19,44 @@ alert("Login Sucessfully");
 }  
 
 
+function validateform1(){  
+var first_name=document.myform.first_name.value;  
+var last_name=document.myform.last_name.value;  
+var feedback=document.myform.feedback.value;  
+  
+  
+if (first_name==null || first_name==""){  
+  alert("can't be blank");  
+  return false;  
+}else if(last_name==null || last_name=="" || feedback==null || feedback==""){  
+  alert("can't be blank.");  
+  
+  return false;  
+  }  
+  else 
+  { 
+alert("Submit your Feedback");
+  }
+  
+}  
+
+
+function validateform2(){  
+var PartyDate=document.myform.PartyDate.value;  
+var people=document.myform.people.value;  
+var description=document.myform.description.value;  
+  
+  if (PartyDate==null || PartyDate==""){  
+  alert("Enter Date");  
+  return false;  
+}else if(people==null || people=="" || description==null || description==""){  
+  alert(" Can't be blank.");  
+  
+  return false;  
+  } 
+  else 
+  { 
+alert("Oder Confirm");
+  }
+  
+}  
